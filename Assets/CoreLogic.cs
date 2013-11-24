e3effca72081316c1e2a8dad6f724ee50135a264
Jacob Hanshaw <hanshaw@wisc.edu>
2013-11-23 23:15:58 -0600

    Stuff

---
+++ Assets/CoreLogic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoreLogic : MonoBehaviour
+{
+
+		bool player2Exists = false;
+	
+		public GameObject playerPrefab;
+		public Material green;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				OuyaSDK.OuyaPlayer player2 = OuyaSDK.OuyaPlayer.player2;
+				if (player2 != null && !player2Exists) {
+						player2Exists = true;
+						GameObject player2Obj = (GameObject)Instantiate (playerPrefab, new Vector3 (0, 10, 0), Quaternion.identity);
+						player2Obj.renderer.material = green;
+						PlayerController controller = player2Obj.GetComponent<PlayerController> ();
+						controller.Index = player2;
+				}
+
+		}
+}
--- Assets/MyAssets/MyScripts/PlayerController.cs
+++ Assets/MyAssets/MyScripts/PlayerController.cs
@@ -8,7 +8,7 @@ OuyaSDK.IMenuButtonUpListener,
 OuyaSDK.IMenuAppearingListener
 {
 	
-		public static OuyaSDK.OuyaPlayer Index = OuyaSDK.OuyaPlayer.player1;
+		public OuyaSDK.OuyaPlayer Index = OuyaSDK.OuyaPlayer.player1;
 	
 	#region New Input API - in progress
 	
@@ -16,6 +16,9 @@ OuyaSDK.IMenuAppearingListener
 	
 	#endregion
 	
+		public TextMesh textMesh;
+		public bool isCurrentlyHeld = false;
+		
 		[HideInInspector]
 		public bool
 				facingRight = true;			// For determining which way the player is currently facing.
@@ -27,9 +30,16 @@ OuyaSDK.IMenuAppearingListener
 		public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
 		public float jumpForce = 1000f;			// Amount of force added when the player jumps.
 
-		private Transform groundCheck;			// A position marking where to check if the player is grounded.
+		private Transform[] groundChecks = new Transform[3];			// A position marking where to check if the player is grounded.
 		private bool grounded = false;			// Whether or not the player is grounded.
-	
+		
+		[HideInInspector]
+		public GameObject
+				carryingPlayer;
+		
+		private BoxCollider2D colliderBox2D;
+		private Vector2 originalColliderSize;
+		private Vector2 originalColliderCenter;
 	
 		void Awake ()
 		{
@@ -39,7 +49,13 @@ OuyaSDK.IMenuAppearingListener
 				OuyaSDK.registerPauseListener (this);
 				OuyaSDK.registerResumeListener (this);
 		
-				groundCheck = transform.Find ("groundCheck");
+				groundChecks [0] = transform.Find ("groundChecka");
+				groundChecks [1] = transform.Find ("groundCheckb");
+				groundChecks [2] = transform.Find ("groundCheckc");
+		
+				colliderBox2D = gameObject.GetComponent<BoxCollider2D> ();
+				originalColliderSize = colliderBox2D.size;
+				originalColliderCenter = colliderBox2D.center;
 		}
 	
 		void OnDestroy ()
@@ -60,16 +76,51 @@ OuyaSDK.IMenuAppearingListener
 		// Update is called once per frame
 		void Update ()
 		{
+				if (carryingPlayer != null) {
+						Vector3 pickedUpPosition = carryingPlayer.transform.position;
+						pickedUpPosition.y += carryingPlayer.transform.localScale.y / 1.5f;
+						pickedUpPosition.y += gameObject.transform.localScale.y / 1.5f;
+						gameObject.transform.position = pickedUpPosition;
+				}
 				//GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_Y, Index) GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_X, Index)
 				//GetAxis(OuyaSDK.KeyEnum.AXIS_RSTICK_Y, Index) GetAxis(OuyaSDK.KeyEnum.AXIS_RSTICK_X, Index)
-	
-				// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-				grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));  
+				
+				foreach (Transform groundCheck in groundChecks)
+						groundCheck.gameObject.SetActive (false);
+			
+				Vector2 centerA = colliderBox2D.center;
+				centerA.y = originalColliderCenter.y + 0.833f * colliderBox2D.size.y;
+				textMesh.text = "Value: " + centerA.y;
+				colliderBox2D.center = centerA;
 		
+				Vector2 size = colliderBox2D.size;
+				size.y = originalColliderSize.y * 2.67f;
+				colliderBox2D.size = size;
+				
+				foreach (Transform groundCheck in groundChecks)
+						groundCheck.gameObject.SetActive (true);
+		
+				// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+				int layerMask = 1 << LayerMask.NameToLayer ("Ground");
+				layerMask |= 1 << LayerMask.NameToLayer ("Player");
+				Vector3 startRaycast = transform.position;
+				startRaycast.y -= transform.localScale.y / 1.99f;
+				grounded = false;
+				foreach (Transform groundCheck in groundChecks)
+						grounded |= Physics2D.Linecast (startRaycast, groundCheck.position, layerMask);
+				
+				//if (Index == OuyaSDK.OuyaPlayer.player1) 
+				//		textMesh.text = "I am grounded: " + grounded;
+					
 				// If the jump button is pressed and the player is grounded then the player should jump.
-				if (GetButton (OuyaSDK.KeyEnum.BUTTON_O, Index) && grounded)
+				if ((GetButton (OuyaSDK.KeyEnum.BUTTON_O, Index) || Input.GetButtonDown ("Jump")) && grounded) {
 						jump = true;
-	
+						isCurrentlyHeld = false;
+						carryingPlayer.GetComponent<PlayerController> ().PlayerLaunched ();
+						carryingPlayer = null;
+						gameObject.rigidbody2D.WakeUp ();
+						//gameObject.rigidbody2D.isKinematic = false;
+				}
 		
 				if (Input.GetButtonDown ("Jump"))
 						Debug.Log ("Jump");
@@ -79,7 +130,8 @@ OuyaSDK.IMenuAppearingListener
 						Debug.Log ("R3");
 				if (GetButton (OuyaSDK.KeyEnum.BUTTON_O, Index)) {
 						Debug.Log ("O");
-
+						//	if (Index == OuyaSDK.OuyaPlayer.player1)
+						//			textMesh.text = "I pressed jump";
 				} 
 				if (GetButton (OuyaSDK.KeyEnum.BUTTON_U, Index)) {
 						Debug.Log ("U");
@@ -101,8 +153,35 @@ OuyaSDK.IMenuAppearingListener
 						Debug.Log ("RB");
 				if (GetButton (OuyaSDK.KeyEnum.BUTTON_LT, Index))
 						Debug.Log ("LT");
-				if (GetButton (OuyaSDK.KeyEnum.BUTTON_RT, Index))
+				if (GetButton (OuyaSDK.KeyEnum.BUTTON_RT, Index) || Input.GetKeyDown ("f")) {
 						Debug.Log ("RT");
+			
+						if (!isCurrentlyHeld) {
+								Collider2D[] hitColliders = Physics2D.OverlapCircleAll (gameObject.transform.position, 0.6f);
+								foreach (Collider2D collider in hitColliders) {
+					
+										if (collider.gameObject != gameObject && collider.gameObject.CompareTag ("Player")) {
+												collider.gameObject.rigidbody2D.Sleep ();
+												//kinetic
+												PlayerController playerController = collider.gameObject.GetComponent<PlayerController> ();
+												playerController.isCurrentlyHeld = true;
+												playerController.carryingPlayer = gameObject;
+												
+												Vector2 center = colliderBox2D.center;
+												center.y = originalColliderCenter.y + 0.833f * colliderBox2D.size.y;
+												colliderBox2D.center = center;
+												
+												Vector2 sizeA = colliderBox2D.size;
+												sizeA.y = originalColliderSize.y * 3.00f;
+												colliderBox2D.size = sizeA;
+																				
+												//	if (Index == OuyaSDK.OuyaPlayer.player1)
+												//			textMesh.text = playerController.Index.ToString ();
+										}
+								}
+						}
+				}
+						
 				if (GetButton (OuyaSDK.KeyEnum.BUTTON_DPAD_UP, Index))
 						Debug.Log ("D_UP");
 				if (GetButton (OuyaSDK.KeyEnum.BUTTON_DPAD_DOWN, Index))
@@ -111,6 +190,12 @@ OuyaSDK.IMenuAppearingListener
 						Debug.Log ("D_LEFT");
 				if (GetButton (OuyaSDK.KeyEnum.BUTTON_DPAD_RIGHT, Index))
 						Debug.Log ("D_RIGHT");
+		}
+		
+		public void PlayerLaunched ()
+		{
+				colliderBox2D.center = originalColliderCenter;
+				colliderBox2D.size = originalColliderSize;
 		}
 	
 		public void OuyaMenuButtonUp ()
@@ -182,36 +267,40 @@ OuyaSDK.IMenuAppearingListener
 	
 		void FixedUpdate ()
 		{
-				// Cache the horizontal input.
-				float h = GetAxis (OuyaSDK.KeyEnum.AXIS_LSTICK_X, Index);	
+				if (!isCurrentlyHeld) {
+						// Cache the horizontal input.
+						float h = GetAxis (OuyaSDK.KeyEnum.AXIS_LSTICK_X, Index);	
 
 	
-				// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-				if (h * rigidbody2D.velocity.x < maxSpeed)
+						// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+						if (h * rigidbody2D.velocity.x < maxSpeed)
 		// ... add a force to the player.
-						rigidbody2D.AddForce (Vector2.right * h * moveForce);
+								rigidbody2D.AddForce (Vector2.right * h * moveForce);
 	
-				// If the player's horizontal velocity is greater than the maxSpeed...
-				if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed)
+						// If the player's horizontal velocity is greater than the maxSpeed...
+						if (Mathf.Abs (rigidbody2D.velocity.x) > maxSpeed)
 		// ... set the player's velocity to the maxSpeed in the x axis.
-						rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+								rigidbody2D.velocity = new Vector2 (Mathf.Sign (rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
 	
-				if (h > 0 && !facingRight)
-						Flip ();
-				else if (h < 0 && facingRight)
-						Flip ();
+						//		if (h > 0 && !facingRight)
+						//				Flip ();
+						//		else if (h < 0 && facingRight)
+						//				Flip ();
 	
+
+				}
+				
 				// If the player should jump...
 				if (jump) {
 						// Add a vertical force to the player.
 						rigidbody2D.AddForce (new Vector2 (0f, jumpForce));
-		
+			
 						// Make sure the player can't jump again until the jump conditions from Update are satisfied.
 						jump = false;
 				}
 		}
-
-
+	
+	
 		void Flip ()
 		{
 				// Switch the way the player is labelled as facing.
